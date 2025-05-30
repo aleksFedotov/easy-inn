3ac5dc789beee7a2afa59557bd915508cd57a588
aleksFedotov <aleksfedotov.dev@gmail.com>
2025-05-30 06:52:17 +0300

    database settings

--- backend/hotelbackend/hotelbackend/settings.py
+++ backend/hotelbackend/hotelbackend/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,  # Таймаут в секундах
+            'init_command': 'PRAGMA busy_timeout=30000;',  # 30 секунд
+        }
     }
 }
 
